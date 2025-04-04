40001ceec620cd58e07bac7122359036dbd0039a
TusharAbhinav <abhinav.tushar@iesoftek.com>
2025-04-05 00:11:45 +0530

    initialize monkey interpreter with lexer and token structures

---
+++ monkey/lexer/lexer.go
@@ -0,0 +1,99 @@
+package lexer
+
+import (
+	token "github.com/TusharAbhinav/monkey/token"
+)
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+func (l *Lexer) NextToken() *token.Token {
+	skipWhitespace(l)
+
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = token.Token{Type: token.ASSIGN, Literal: "="}
+	case '+':
+		tok = token.Token{Type: token.PLUS, Literal: "+"}
+	case '(':
+		tok = token.Token{Type: token.LPAREN, Literal: "("}
+	case ')':
+		tok = token.Token{Type: token.RPAREN, Literal: ")"}
+	case '{':
+		tok = token.Token{Type: token.LBRACE, Literal: "{"}
+	case '}':
+		tok = token.Token{Type: token.RBRACE, Literal: "}"}
+	case ',':
+		tok = token.Token{Type: token.COMMA, Literal: ","}
+	case ';':
+		tok = token.Token{Type: token.SEMICOLON, Literal: ";"}
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.ReadKeyword(tok.Literal)
+			return &tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return &tok
+		} else {
+			tok = token.Token{Type: token.ILLEGAL, Literal: string(l.ch)}
+		}
+	}
+	l.readChar()
+	return &tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	start := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[start:l.position]
+}
+func (l *Lexer) readNumber() string {
+	start := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[start:l.position]
+}
+func skipWhitespace(l *Lexer) {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+func isDigit(ch uint8) bool {
+	if ch >= '0' && ch <= '9' {
+		return true
+	}
+	return false
+}
+func isLetter(ch uint8) bool {
+	if ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_' {
+		return true
+	}
+	return false
+}
---
+++ monkey/token/token.go
@@ -0,0 +1,47 @@
+package Token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"let": LET,
+	"fn":  FUNCTION,
+}
+
+func ReadKeyword(input string) TokenType {
+	val, ok := keywords[input]
+	if ok {
+		return val
+	}
+	return IDENT
+}
